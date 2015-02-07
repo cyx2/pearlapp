959c766ef3aaf1245f8c84569e84e66c57e868bc
Charlie Xu <xucharliey@gmail.com>
2015-02-07 17:17:10 -0500

    fixed pages controller..?  Again?

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,13 +3,11 @@ class PagesController < ApplicationController
   	if user_signed_in?
   		@lastratings=Rating.last(5)
   		@userlastratings=current_user.ratings.last(5)
-  		#@var=Cornellclass.where()
   		@count=Cornellclass.where("avgrating >= ?", 1).count
       if @count > 5
         @count=5
       end
-  		@highestratedclasses=Cornellclass.order("avgrating DESC")
-      #.take(@count)
+  		@highestratedclasses=Cornellclass.where("avgrating >= ?", 1).order("avgrating DESC").take(@count)
   	end
   end
   
