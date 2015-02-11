5750cf9e9b4e48eeaac776651c0202cd5eee53a8
Linda Gao <ljg98@cornell.edu>
2015-02-10 22:49:43 -0500

    Fixed to search with autocomplete

--- app/controllers/cornellclasses_controller.rb
+++ app/controllers/cornellclasses_controller.rb
@@ -18,37 +18,46 @@ class CornellclassesController < ApplicationController
     # Finds search results from page/home search form, paginates
     search_text = params[:search_text]
 
-    prefix = ""
-    coursenumber = ""
-    freeform = ""
-
-    i = 0
-    n = search_text.length
-    parse_pre = true
-    parse_post = false
-    while i < n do
-      if (parse_pre && search_text[i] =~ /[A-Za-z ]/)
-        prefix = prefix + search_text[i]
-        puts(search_text[i])
-      elsif (!parse_post && search_text[i] =~ /[0-9]/)
-        parse_pre = false
-        coursenumber = coursenumber + search_text[i]
-      else
-        parse_post = true 
-        freeform = freeform + search_text[i]
-      end
-      i = i + 1
-    end
-
-    if (coursenumber == "" && !Cornellclass.exists?(['UPPER(prefix) = UPPER(?)', prefix.strip]))
-      freeform = prefix
-      prefix = ""
+    if (search_text.include?(':') && (search_text.split(': ')).length == 2)
+      parsed_search = search_text.split(': ')
+      prefix = parsed_search.first.split(' ').first
+      coursenumber = parsed_search.first.split(' ').last
+      freeform = search_text.last
+      @search_results = Cornellclass.where("UPPER(prefix) LIKE '%' || UPPER(?) || '%' AND coursenumber = ? AND lower(title) LIKE '%' || lower(?) || '%'", prefix, coursenumber.to_i, freeform).paginate(:page => params[:page], :per_page => 50)
+      respond_with(@search_results)
     else
-      prefix = prefix.strip
+      prefix = ""
+      coursenumber = ""
+      freeform = ""
+
+      i = 0
+      n = search_text.length
+      parse_pre = true
+      parse_post = false
+      while i < n do
+        if (parse_pre && search_text[i] =~ /[A-Za-z ]/)
+          prefix = prefix + search_text[i]
+          puts(search_text[i])
+        elsif (!parse_post && search_text[i] =~ /[0-9]/)
+          parse_pre = false
+          coursenumber = coursenumber + search_text[i]
+        else
+          parse_post = true 
+          freeform = freeform + search_text[i]
+        end
+        i = i + 1
+      end
+
+      if (coursenumber == "" && !Cornellclass.exists?(['UPPER(prefix) = UPPER(?)', prefix.strip]))
+        freeform = prefix
+        prefix = ""
+      else
+        prefix = prefix.strip
+      end
+      
+      @search_results = Cornellclass.where("UPPER(prefix) LIKE '%' || UPPER(?) || '%' AND (coursenumber = ? OR 0 = ?) AND (lower(title) LIKE '%' || lower(?) || '%' OR lower(instructor) LIKE '%' || lower(?) || '%')", prefix, coursenumber.to_i, coursenumber.to_i, freeform, freeform).paginate(:page => params[:page], :per_page => 50)
+      respond_with(@search_results)
     end
-    
-    @search_results = Cornellclass.where("UPPER(prefix) LIKE '%' || UPPER(?) || '%' AND (coursenumber = ? OR 0 = ?) AND (lower(title) LIKE '%' || lower(?) || '%' OR lower(instructor) LIKE '%' || lower(?) || '%')", prefix, coursenumber.to_i, coursenumber.to_i, freeform, freeform).paginate(:page => params[:page], :per_page => 50)
-    respond_with(@search_results)
   end
 
   def show
