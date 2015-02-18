e8e2d2766df9efc1814b388dbd7a85d238683e92
Linda Gao <ljg98@cornell.edu>
2015-02-17 23:07:08 -0500

    Autocomplete on ratings page

--- app/controllers/ratings_controller.rb
+++ app/controllers/ratings_controller.rb
@@ -33,45 +33,54 @@ class RatingsController < ApplicationController
   end
 
   def create
-    # Append courseid to each rating    
-    @cornell_classes = Cornellclass.where("UPPER(prefix) = UPPER(?) AND coursenumber = ?",
-     rating_params[:prefix].strip, rating_params[:course_number].strip)
-    @cornell_classes.each do |cornell_class| 
-      # cornell_class is a single class
-      with_id_params = rating_params
-      with_id_params[:courseid] = cornell_class.courseid
-      # Append user_id to each rating
-      @rating = current_user.ratings.build(with_id_params)
-      @rating.save
-      @rating.format      
-      # Aggregate data calculation      
-      # Context calculation
-      cornell_class.countratings 
-      # Boolean avg calculation ## Must be above others
-      # because numerical calc functions depend on bools
-      cornell_class.calchwyesno
-      cornell_class.calcrecitationreqdyesno
-      cornell_class.calcexamyesno
-      cornell_class.calclecturereqdyesno
-      cornell_class.calcprojyesno      
-      cornell_class.calcprelimyesno
-      cornell_class.calcpaperyesno 
-      # Quality avg calculation
-      cornell_class.calcavgrating
-      cornell_class.calcprofqual
-      cornell_class.calctaqual
-      cornell_class.calclecturequal
-      cornell_class.calcrecitationqual
-      # Difficulty avg calculation
-      cornell_class.calchwdiff
-      cornell_class.calcexamdiff
-      cornell_class.calcmaterialdiff
-      cornell_class.calcprojdiff
-      cornell_class.calcprelimdiff
-      cornell_class.calcpaperdiff
+    # Append courseid to each rating
+    search_text = rating_params[:search_text]
+    if (search_text.include?(':') && (search_text.split(': ')).length == 2)
+      parsed_search = search_text.split(': ')
+      prefix = parsed_search.first.split(' ').first
+      coursenumber = parsed_search.first.split(' ').last
+      freeform = search_text.last
+      @cornell_classes = Cornellclass.where("UPPER(prefix) = UPPER(?) AND coursenumber = ?", prefix, coursenumber)
+      @cornell_classes.each do |cornell_class| 
+        # cornell_class is a single class
+        with_id_params = rating_params
+        with_id_params.delete :search_text
+        with_id_params[:prefix] = prefix
+        with_id_params[:course_number] = coursenumber
+        with_id_params[:courseid] = cornell_class.courseid
+        # Append user_id to each rating
+        @rating = current_user.ratings.build(with_id_params)
+        @rating.save
+        @rating.format      
+        # Aggregate data calculation      
+        # Context calculation
+        cornell_class.countratings 
+        # Boolean avg calculation ## Must be above others
+        # because numerical calc functions depend on bools
+        cornell_class.calchwyesno
+        cornell_class.calcrecitationreqdyesno
+        cornell_class.calcexamyesno
+        cornell_class.calclecturereqdyesno
+        cornell_class.calcprojyesno      
+        cornell_class.calcprelimyesno
+        cornell_class.calcpaperyesno 
+        # Quality avg calculation
+        cornell_class.calcavgrating
+        cornell_class.calcprofqual
+        cornell_class.calctaqual
+        cornell_class.calclecturequal
+        cornell_class.calcrecitationqual
+        # Difficulty avg calculation
+        cornell_class.calchwdiff
+        cornell_class.calcexamdiff
+        cornell_class.calcmaterialdiff
+        cornell_class.calcprojdiff
+        cornell_class.calcprelimdiff
+        cornell_class.calcpaperdiff
+        respond_with(@rating) unless @cornell_classes.first.nil?    
+        redirect_to (:back), notice: "No class found with that prefix and number" if @cornell_classes.first.nil?
+      end
     end
-    respond_with(@rating) unless @cornell_classes.first.nil?    
-    redirect_to (:back), notice: "No class found with that prefix and number" if @cornell_classes.first.nil?
   end
 
   def update
@@ -96,6 +105,6 @@ class RatingsController < ApplicationController
     end
 
     def rating_params
-      params.require(:rating).permit(:rater, :rating, :created_at, :updated_at, :prefix, :course_number, :hwyesno, :recitationreqdyesno, :examyesno, :lecturereqdyesno, :projyesno, :profqual, :taqual, :lecturequal, :recitationqual, :hwdiff, :examdiff, :materialdiff, :projdiff, :prelimyesno, :prelimdiff, :semester, :paperyesno, :paperdiff)
+      params.require(:rating).permit(:rater, :rating, :created_at, :updated_at, :prefix, :course_number, :search_text, :hwyesno, :recitationreqdyesno, :examyesno, :lecturereqdyesno, :projyesno, :profqual, :taqual, :lecturequal, :recitationqual, :hwdiff, :examdiff, :materialdiff, :projdiff, :prelimyesno, :prelimdiff, :semester, :paperyesno, :paperdiff)
     end
 end
