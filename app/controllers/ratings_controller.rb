fd6086af20c093670d73a854e2aacf6d9b1274e4
Linda Gao <ljg98@cornell.edu>
2015-02-27 20:18:32 -0500

    Added if statement to chck for empty class when rating

--- app/controllers/ratings_controller.rb
+++ app/controllers/ratings_controller.rb
@@ -37,47 +37,51 @@ class RatingsController < ApplicationController
   def create
 
     if !rating_params[:search_text].present?
-      # Append user_id to each rating
-      @rating = current_user.ratings.build(rating_params)
-      @rating.save
-      @rating.format
+      if (rating_params[:prefix].present? && rating_params[:coursenumber].present?)
+        # Append user_id to each rating
+        @rating = current_user.ratings.build(rating_params)
+        @rating.save
+        @rating.format
 
-      @cornell_classes = Cornellclass.where("UPPER(prefix) = UPPER(?) AND coursenumber = ?", rating_params[:prefix], rating_params[:course_number])
-      @cornell_classes.each do |cornell_class|
-        # Aggregate data calculation      
-        # Context calculation
-        cornell_class.countratings 
-        # Boolean avg calculation ## Must be above others
-        # because numerical calc functions depend on bools
-        cornell_class.calchwyesno
-        cornell_class.calcrecitationreqdyesno
-        cornell_class.calcexamyesno
-        cornell_class.calclecturereqdyesno
-        cornell_class.calcprojyesno      
-        cornell_class.calcprelimyesno
-        cornell_class.calcpaperyesno 
-        cornell_class.calclabyesno
-        # Quality avg calculation
-        cornell_class.calcavgrating
-        cornell_class.calcprofqual
-        cornell_class.calctaqual
-        cornell_class.calclecturequal
-        cornell_class.calcrecitationqual
-        cornell_class.calclabqual
-        # Difficulty avg calculation
-        cornell_class.calchwdiff
-        cornell_class.calcexamdiff
-        cornell_class.calcmaterialdiff
-        cornell_class.calcprojdiff
-        cornell_class.calcprelimdiff
-        cornell_class.calcpaperdiff    
+        @cornell_classes = Cornellclass.where("UPPER(prefix) = UPPER(?) AND coursenumber = ?", rating_params[:prefix], rating_params[:course_number])
+        @cornell_classes.each do |cornell_class|
+          # Aggregate data calculation      
+          # Context calculation
+          cornell_class.countratings 
+          # Boolean avg calculation ## Must be above others
+          # because numerical calc functions depend on bools
+          cornell_class.calchwyesno
+          cornell_class.calcrecitationreqdyesno
+          cornell_class.calcexamyesno
+          cornell_class.calclecturereqdyesno
+          cornell_class.calcprojyesno      
+          cornell_class.calcprelimyesno
+          cornell_class.calcpaperyesno 
+          cornell_class.calclabyesno
+          # Quality avg calculation
+          cornell_class.calcavgrating
+          cornell_class.calcprofqual
+          cornell_class.calctaqual
+          cornell_class.calclecturequal
+          cornell_class.calcrecitationqual
+          cornell_class.calclabqual
+          # Difficulty avg calculation
+          cornell_class.calchwdiff
+          cornell_class.calcexamdiff
+          cornell_class.calcmaterialdiff
+          cornell_class.calcprojdiff
+          cornell_class.calcprelimdiff
+          cornell_class.calcpaperdiff    
 
-        cornell_class.calcavggrade
-        
-        respond_with(@rating) unless @cornell_classes.nil?    
-        redirect_to (:back), notice: "No class found with that prefix and number" if @cornell_classes.nil?
+          cornell_class.calcavggrade
+          
+          respond_with(@rating) unless @cornell_classes.nil?    
+          redirect_to (:back), notice: "No class found with that prefix and number" if @cornell_classes.nil?
+        end
+      else
+        redirect_to (:back), notice: "No class prefix or number entered"
       end
-      # respond_with(@rating) unless @cornell_classes.nil?
+        # respond_with(@rating) unless @cornell_classes.nil?
     else
       # Append courseid to each rating
       search_text = rating_params[:search_text]
